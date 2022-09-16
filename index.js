cdcc486bd3a5eecd05bc754aadd72f60c6b3ed15
DEREK-DEV-AFK <sufiyanmemon687@gmail.com>
2022-09-16 13:17:33 +0530

    added comments and modified file name

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const provider = new ethers.providers.JsonRpcProvider(
     data.apiKey.goerli.alchemy.url
 );
 
+// connecting first add as signer 1 and other as signer 2
 const signer1 = new ethers.Wallet(data.accounts.acc1["private-key"], provider);
 const signer2 = new ethers.Wallet(data.accounts.acc2["private-key"], provider);
 
+
+// implemented two seaports obj for different signers
 const seaport1 = new Seaport(signer1);
 const seaport2 = new Seaport(signer2);
-/// offering nad fullfillin it
 
+
+/// offering nad fullfillin it
 async function main() {
-    const offerer = data.accounts.acc1.address;
-    const fulfiller = data.accounts.acc2.address;
+    const offerer = data.accounts.acc1.address; // offerer address which is going to offer
+    const fulfiller = data.accounts.acc2.address; // fulfiller address which is going to accept or fullfill
     console.log("creating order !");
+     // creating order by signer 1
     const { executeAllActions } = await seaport1.createOrder(
         {
             offer: [
@@ -29,8 +34,8 @@ async function main() {
             ],
             consideration: [
                 {
-                    amount: ethers.utils.parseEther("0.1").toString(),
-                    recipient: offerer,
+                    amount: ethers.utils.parseEther("0.1").toString(), // in return we are asking for 0.1 eth
+                    recipient: offerer, // and the recipient must be offerer
                 },
             ],
         },
@@ -38,16 +43,17 @@ async function main() {
     );
     console.log("created order success");
 
-    const order = await executeAllActions();
+    const order = await executeAllActions(); // getting the order array to pass in fullfill block
     console.log("order : ", order)
 
+    // this will be call by signer 1 to fullfill the order
     const { executeAllActions: executeAllFulfillActions } =
         await seaport2.fulfillOrder({
-            order,
-            accountAddress: fulfiller,
+            order, // providing ther order which we are fullfilling 
+            accountAddress: fulfiller, // the address through which we will fulfill
         });
 
-    const transaction = await executeAllFulfillActions();
+    const transaction = await executeAllFulfillActions(); // getting the transaction details
 
     console.log("transaction success");
     console.log(transaction);
